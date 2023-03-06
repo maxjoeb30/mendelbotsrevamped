99f3ea4f1c9e4b2bb72a2b82c4ac7f7c27c69d98
Max Bradford <mjbradfo@uvm.edu>
2023-03-05 23:24:15 -0500

    Stepping into the simulation.

--- simulate.py
+++ simulate.py
@@ -1,8 +1,23 @@
 #Importing necessary packages
 import pybullet as p
+import time
 
 
-#Setting up the physics client
+#Setting up the physics client, connect to simulation
 physicsClient = p.connect(p.GUI)
+
+#Stepping through physics engine at a time delay with a for loop
+
+#Create a list for use in the for loop
+forlooplist = 1000
+
+for i in range(forlooplist):
+    p.stepSimulation()
+    time.sleep(1/60)
+    print(i)
+    
+#Disconnect from the simulation
 p.disconnect()
+
+
 
