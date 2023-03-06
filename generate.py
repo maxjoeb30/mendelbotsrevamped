396637d61d06e0885a82e5c51b59339961a3f867
Max Bradford <mjbradfo@uvm.edu>
2023-03-06 13:15:37 -0500

    Committing procedural link generation

--- generate.py
+++ generate.py
@@ -2,11 +2,53 @@
 #Import necessary packages
 import pyrosim.pyrosim as pyrosim
 
-#Define the file where the world information is stored
-pyrosim.Start_SDF("box.sdf")
+#Define the file output where the world information is stored
+pyrosim.Start_SDF("boxes.sdf")
 
-#Generate a cube in the world
-pyrosim.Send_Cube(name="Box", pos= [0,0,0.5] , size = [1,1,1])
+#Generate cubes in the world with variables as l,w,h and position variables as x,y,z
+
+#First box variables
+length = 1
+width = 1
+height = 1
+#
+x = 1
+y = 1
+z = .5
+
+
+#Cube generation 1
+#pyrosim.Send_Cube(name="Box", pos= [x, y, z], size = [length,width,height])
+
+#Cube generation 2
+#pyrosim.Send_Cube(name="Box2", pos= [x2, y2, z2], size = [length2,width2,height2])
+
+
+#Procedural generation of links
+forlistgenloop = 11
+forlistgenloop2 = 6
+forlistgenloop3 = 6
+for i in range(forlistgenloop3):
+    for i in range(forlistgenloop2):
+
+        #Reset the length, width, height fields    
+        length = 1
+        width = 1 
+        height = 1
+
+        for i in range(forlistgenloop):
+            pyrosim.Send_Cube(name="Box", pos= [x, y, z], size = [length,width,height])
+            z = z + 1
+            length = length * .9
+            width = width * .9
+            height = height * .9
+
+        x = x + 1 
+    y = y + 1
+
+
+
+
 
 
 
--- simulate.py
+++ simulate.py
@@ -1,20 +1,29 @@
 #Importing necessary packages
 import pybullet as p
+import pybullet_data
 import time
 
 
 #Setting up the physics client, connect to simulation
 physicsClient = p.connect(p.GUI)
 
-#Stepping through physics engine at a time delay with a for loop
+#Offer access to pybullet data
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
 
 #Create a list for use in the for loop
 forlooplist = 1000
+
+#Setting specifics of the environment
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+
 
 #Read in the simulated world environment from box.sdf
 p.loadSDF("box.sdf")
 
 
+#Stepping through physics engine at a time delay with a for loop
 for i in range(forlooplist):
     p.stepSimulation()
     time.sleep(1/60)
